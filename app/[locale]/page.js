318c4e7d5941c9653ef57e4efd6be2392a43f4fd
alastairandress <63905089+alastairandress@users.noreply.github.com>
2026-04-24 16:27:13 +0100

    Implement locale handling in Home component
    Add locale support for static rendering in Home component.

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,3 +1,4 @@
+import { unstable_setRequestLocale } from 'next-intl/server';
 import Navbar from '../../components/Navbar';
 import Hero from '../../components/Hero';
 import HowItWorks from '../../components/HowItWorks';
@@ -8,7 +9,10 @@ import QuoteForm from '../../components/QuoteForm';
 import CtaBanner from '../../components/CtaBanner';
 import Footer from '../../components/Footer';
 
-export default function Home() {
+export default function Home({ params: { locale } }) {
+  // Enable static rendering for this locale
+  unstable_setRequestLocale(locale);
+
   return (
     <main className="min-h-screen bg-white">
       <Navbar />
